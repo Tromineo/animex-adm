847397d54bb7261d76e818b37970f2a476021099
Monteiro <rms_monteirosteil@hotmail.com>
2024-09-14 09:45:24 -0300

    Implementando componentes resumo e persoangens

---
+++ episodio.php
@@ -0,0 +1,56 @@
+<?php include('header.php');?>
+<style>
+    .border-red{
+        border: solid 1px red;
+    }
+</style>
+<main>
+    <div class="container border-red">
+        <div class="row">
+            <div class="col-lg-12 mt-2">
+                <h3 class="text-center">Titulo episodio</h3>
+                
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/img/placeholder.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-9">
+              <div class="row">
+                <div class="col-lg-12">
+                    <h4>Resumo</h4>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus omnis reiciendis qui dolore recusandae beatae consequuntur, earum molestias est rerum et sed, nobis distinctio minima autem laborum voluptatum animi ut?</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-12 mt-2">
+                    <h4>Personagens</h4>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item">item</li>
+                        <li class="list-group-item">item</li>
+                        <li class="list-group-item">item</li>
+                    </ul>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</main>
